2dac306c03e96cb4166a3ae50d6213e54157d365
Helen <elena.kozovyakina@gmail.com>
2024-07-27 13:00:18 +0300

    blog

--- fifth/programming_blog/blog/admin.py
+++ fifth/programming_blog/blog/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 from .models import *
 
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+from django import forms
+
+
+class BlogAdminForm(forms.ModelForm):
+    content = forms.CharField(widget=CKEditorUploadingWidget())
+
 
 class BlogAdmin(admin.ModelAdmin):
+    form = BlogAdminForm
     prepopulated_fields = {'slug': ('title',)}
     list_display = ('id', 'title', 'time_created', 'photo', 'is_published')
     list_display_links = ('id', 'title')
